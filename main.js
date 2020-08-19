8b51e2965ad7f02ff4548b4e5f93d56986391759
jenross <ross.m.jennifer@gmail.com>
2020-08-19 15:57:53 -0400

    started adding questions

--- main.js
+++ main.js
@@ -4,61 +4,61 @@
 //   console.log(numberArray[i]);
 // }
 
-const favoriteFoods = {
-  breakfast: "bacon and eggs",
-  lunch: "pizza and salad",
-  dinner: "lasagne",
-  dessert: "ice cream",
-};
+// const favoriteFoods = {
+//   breakfast: "bacon and eggs",
+//   lunch: "pizza and salad",
+//   dinner: "lasagne",
+//   dessert: "ice cream",
+// };
 
-const breakfastCard = `
-<div class="card" style="width: 18rem;">
-<div class="card-body">
-  <h5 class="card-title">${favoriteFoods.breakfast}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">Feed me now!</h6>
-  <p class="card-text"></p>
-  <a href="#" class="card-link">Card link</a>
-  <a href="#" class="card-link">Another link</a>
-</div>
-</div>
-`;
+// const breakfastCard = `
+// <div class="card" style="width: 18rem;">
+// <div class="card-body">
+//   <h5 class="card-title">${favoriteFoods.breakfast}</h5>
+//   <h6 class="card-subtitle mb-2 text-muted">Feed me now!</h6>
+//   <p class="card-text"></p>
+//   <a href="#" class="card-link">Card link</a>
+//   <a href="#" class="card-link">Another link</a>
+// </div>
+// </div>
+// `;
 
-const questions = [
-  {
-    question: "What does Leslie like to eat?",
-    answerChoices: ["Waffles", "Ice Cream", "asparagus", "steak"],
-    answer: "Waffles",
-  },
-  {
-    name: "Ron Swanson",
-    location: "Pawnee, Indiana",
-    loves: "silence",
-  },
-  {
-    name: "April Ludgate",
-    location: "Pawnee, Indiana",
-    loves: "Halloween",
-  },
-  {
-    name: "Tom Haverford",
-    location: "Pawnee, Indiana",
-    loves: "a lot of things",
-  },
-];
+// const questions = [
+//   {
+//     question: "What does Leslie like to eat?",
+//     answerChoices: ["Waffles", "Ice Cream", "asparagus", "steak"],
+//     answer: "Waffles",
+//   },
+//   {
+//     name: "Ron Swanson",
+//     location: "Pawnee, Indiana",
+//     loves: "silence",
+//   },
+//   {
+//     name: "April Ludgate",
+//     location: "Pawnee, Indiana",
+//     loves: "Halloween",
+//   },
+//   {
+//     name: "Tom Haverford",
+//     location: "Pawnee, Indiana",
+//     loves: "a lot of things",
+//   },
+// ];
 
-for (let i = 0; i < characters.length; i++) {
-  const characterCard = `
-  <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${characters[i].name}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">${characters[i].location}</h6>
-    <p class="card-text">I love ${characters[i].loves}.</p>
+// for (let i = 0; i < characters.length; i++) {
+//   const characterCard = `
+//   <div class="card" style="width: 18rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">${characters[i].name}</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">${characters[i].location}</h6>
+//     <p class="card-text">I love ${characters[i].loves}.</p>
     
-  </div>
-  </div>
-  `;
-  $(".col").append(characterCard);
-}
+//   </div>
+//   </div>
+//   `;
+//   $(".col").append(characterCard);
+// }
 
 // $(".col").append(breakfastCard);
 
